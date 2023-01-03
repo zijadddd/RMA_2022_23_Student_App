a37eac3fc90073dc75d683f6c803cf1ed1b61754
zijadddd <zijad.doglod.20@size.ba>
2023-01-03 17:52:34 +0100

    git push --all origin

--- RMA_2022_23_Student_App/ProfilePage.xaml.cs
+++ RMA_2022_23_Student_App/ProfilePage.xaml.cs
@@ -10,8 +10,8 @@ public partial class ProfilePage : ContentPage
 
 		String temp = "";
 		String[] dates = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		DateTime todayDate = new DateTime();
-		temp += dates[todayDate.Month] + " " + todayDate.Day;
+		DateTimeOffset todayDate = DateTimeOffset.Now;
+		temp += dates[todayDate.Month - 1] + " " + todayDate.Day;
 		if (todayDate.Day%10 == 1 && todayDate.Day != 11) temp += "st";
 		else if (todayDate.Day%10 == 2 && todayDate.Day != 12) temp += "nd";
 		else if (todayDate.Day%10 == 3 && todayDate.Day != 13) temp += "rd";
